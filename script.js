d9e8d6063bf4864a0913cfe51a0369812d6865dd
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-06 22:12:43 +0300

    made the grid be adjustable and fill the screen height-wise

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const container = document.querySelector(".container");
-const cellCount = 16
+const cellCount = 16;
 
-for (let y = 0; y < cellCount; y++) {
-    const row = document.createElement('div');
-    row.classList.add("row")
 
-    for (let x = 0; x < cellCount; x++) {
-        const cell = document.createElement('div');
-        cell.classList.add("cell");
-        row.appendChild(cell);
+createGrid(cellCount);
+
+function createGrid(gridSize) {
+    for (let y = 0; y < gridSize; y++) {
+        const row = document.createElement('div');
+        row.classList.add("row")
+
+        for (let x = 0; x < gridSize; x++) {
+            const cell = document.createElement('div');
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+
+        container.appendChild(row);
     }
-
-    container.appendChild(row);
 }
