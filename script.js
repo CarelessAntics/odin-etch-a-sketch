ad60d6c2181faf78138b341dfecf81a08e84d444
Antti Heikkinen <6470546+CarelessAntics@users.noreply.github.com>
2026-05-07 00:47:32 +0300

    Removed fade from hover. Added checkboxes to toggle randomization and darkening

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const container = document.querySelector(".draw-container");
-const cellCount = 16;
 const slider = document.querySelector('#gridSize');
 const output = document.querySelector('#sizeOut');
+const chkDarken = document.querySelector('#darken')
+const chkRandomize = document.querySelector('#randomize')
 output.textContent = `Resolution: ${slider.value}`;
 
-// Extra credit: darkening effect:
 container.addEventListener('mouseover', function (event) {
     event.stopPropagation();
     if (event.target === this) return;
 
-    event.target.style.opacity -= .1;
-})
+    // Extra credit: Darken the squares while drawing
+    if (chkDarken.checked) event.target.style.opacity -= .1;
+
+    event.target.classList.add("draw");
+});
 
 
 createGrid(slider.value);
@@ -21,6 +24,11 @@ slider.oninput = function () {
     output.textContent = `Resolution: ${this.value}`;
 };
 
+chkRandomize.onchange = function () {
+    destroyGrid();
+    createGrid(slider.value);
+}
+
 function createGrid(gridSize) {
     for (let y = 0; y < gridSize; y++) {
         const row = document.createElement('div');
@@ -30,9 +38,16 @@ function createGrid(gridSize) {
             const cell = document.createElement('div');
             cell.classList.add("cell");
 
-            let R = Math.floor(Math.random() * 256);
-            let G = Math.floor(Math.random() * 256);
-            let B = Math.floor(Math.random() * 256);
+            // TODO? implement a colorpicker maybe someday
+            let R = 104;
+            let G = 255;
+            let B = 204;
+
+            if (chkRandomize.checked) {
+                R = Math.floor(Math.random() * 256);
+                G = Math.floor(Math.random() * 256);
+                B = Math.floor(Math.random() * 256);
+            }
 
             cell.style.setProperty('--rnd-background', `rgb(${R}, ${G}, ${B})`);
             cell.style.opacity = 1;
